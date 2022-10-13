a872925ae0c6f4b0584c9517e62c8e747ed62b12
MatijaFabris <75133090+Matijaaaa@users.noreply.github.com>
2022-10-13 12:21:36 +0200

    Napravljena stranica za odabir boje

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
     <div class="mt-8 flex justify-center">
-      <h1 class="text-8xl font-semibold uppercase">Pitura</h1>
+      <h1 class="text-8xl font-semibold uppercase naslov">Pitura</h1>
     </div>
     <div class="flex justify-center">
       <h1 class="text-lg font-semibold">Aplikacija za pomoć pri farbanju</h1>
@@ -27,4 +27,22 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Rancho&display=swap');
+.naslov{
+  font-family: 'Rancho';
+font-style: normal;
+font-weight: 400;
+font-size: 110px;
+line-height: 112px;
+/* identical to box height, or 117% */
+
+letter-spacing: -1.5px;
+
+/* 00. On Surface / High Emphasis */
+
+color: rgba(0, 0, 0, 0.87);
+
+text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+</style>
--- src/views/odabir-boje-view.vue
+++ src/views/odabir-boje-view.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="about">
-    <h1>Odabir boje</h1>
-  </div>
+  <v-container>
+    <div class="mt-8 flex justify-center">
+      <h1 class="text-3xl font-semibold">Odaberite vrstu boje</h1>
+
+    </div>
+    <div class="mt-8 flex justify-center">
+      <v-select
+          :items="vrstaBoje"
+          label="Odabir boje"
+          dense
+          outlined
+          class="inline"
+        ></v-select>
+    </div>
+    <div class="mt-8 flex justify-center">
+      <h1 class="text-xl font-semibold">Odaberite boju</h1>
+    </div>
+    <div class="mt-8 flex justify-center">
+      <v-color-picker
+        dot-size="25"
+        swatches-max-height="200"
+      ></v-color-picker>
+  
+    </div>
+</v-container>
 </template>
+
+<script>
+export default {
+  name: "odabir-boje",
+  data(){
+    return{
+      vrstaBoje: [
+        "Boja za metal",
+        "Boja za drvo",
+        "Boja za beton",
+        "Boja za laminat",
+        "Boja za fasadu",
+        "Boja za interijer",
+        "Boja za podvodnu namjenu",
+        "Boja za plastiku",
+        "Boja za auto"
+      ],
+      
+    }   
+  }
+};
+
+</script>
+
+<style>
+.inline {
+    max-width: 300px;
+}
+</style>
