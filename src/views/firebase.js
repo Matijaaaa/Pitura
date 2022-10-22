2ec8707a2a90e91337d8eb8452c12af8552e69e0
MatijaFabris <75133090+Matijaaaa@users.noreply.github.com>
2022-10-22 21:21:30 +0200

    Napravljen stranica za registrciju, Firebase

--- src/views/firebase.js
+++ src/views/firebase.js
@@ -1,3 +1,17 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDHjOqbDjA_Zbt8o2BRBcHmaVz7A5HIJoQ",
+  authDomain: "pitura-81d47.firebaseapp.com",
+  projectId: "pitura-81d47",
+  storageBucket: "pitura-81d47.appspot.com",
+  messagingSenderId: "59453422665",
+  appId: "1:59453422665:web:1e30f9b486cd83069a5398",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
--- src/views/registracija-view.vue
+++ src/views/registracija-view.vue
@@ -1,44 +1,50 @@
 <template>
   <div>
-    <div class="mt-8 flex justify-center">
-      <h1 class="text-2xl font-semibold">Registracija korisnika</h1>
-    </div>
-    <div class="mt-8 flex justify-center">
-      <v-text-field
-        class="inline"
-        label="E-mail"
-        :rules="rules"
-        hide-details="auto"
-      ></v-text-field>
-    </div>
-    <div class="mt-8 flex justify-center">
-      <v-text-field
-        :append-icon="show3 ? 'mdi-eye' : 'mdi-eye-off'"
-        :rules="[passwordRules.required, passwordRules.min]"
-        :type="show3 ? 'text' : 'password'"
-        name="input-10-2"
-        label="Lozinka"
-        hint="Najmanje 8 znakova"
-        class="input-group--focused inline"
-        @click:append="show3 = !show3"
-      ></v-text-field>
-    </div>
-    <div class="mt-8 flex justify-center">
-      <button
-        class="bg-purple-700 hover:bg-purple-500 text-white py-2 px-4 rounded"
-        @click.prevent="saveData"
-        :disabled="!izracunLitara"
-      >
-        Registracija
-      </button>
-    </div>
-    <div class="mt-8 flex justify-center">
-      <p>Imaš račun? <a href="/prijava">Prijavi se!</a></p>
-    </div>
+    <form>
+      <div class="mt-8 flex justify-center">
+        <h1 class="text-2xl font-semibold">Registracija korisnika</h1>
+      </div>
+      <div class="mt-8 flex justify-center">
+        <v-text-field
+          class="inline"
+          v-model="email"
+          label="E-mail"
+          :rules="rules"
+          hide-details="auto"
+        ></v-text-field>
+      </div>
+      <div class="mt-8 flex justify-center">
+        <v-text-field
+          :append-icon="show3 ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[passwordRules.required, passwordRules.min]"
+          :type="show3 ? 'text' : 'password'"
+          v-model="password"
+          name="input-10-2"
+          label="Lozinka"
+          hint="Najmanje 8 znakova"
+          class="input-group--focused inline"
+          @click:append="show3 = !show3"
+        ></v-text-field>
+      </div>
+      <div class="mt-8 flex justify-center">
+        <button
+          type="button"
+          @click="signup"
+          class="bg-purple-700 hover:bg-purple-500 text-white py-2 px-4 rounded"
+        >
+          Registracija
+        </button>
+      </div>
+      <div class="mt-8 flex justify-center">
+        <p>Imaš račun? <a href="/prijava">Prijavi se!</a></p>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import { firebase } from "..//views/firebase.js";
+
 export default {
   data: () => ({
     rules: [
@@ -48,13 +54,25 @@ export default {
     ],
     show1: false,
     show3: false,
-    password: "Password",
+    email: "",
+    password: "",
     passwordRules: {
-      required: (value) => !!value || "Required.",
-      min: (v) => v.length >= 8 || "Min 8 characters",
+      required: (value) => !!value || "Obavezno.",
+      min: (v) => v.length >= 8 || "Min 8 znakova",
       emailMatch: () => `The email and password you entered don't match`,
     },
   }),
+  methods: {
+    signup() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(function () {
+          console.log("Uspješna registracija");
+        });
+      console.log("Nastavak");
+    },
+  },
 };
 </script>
 
